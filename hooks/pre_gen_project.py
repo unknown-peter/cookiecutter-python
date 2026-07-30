27fb472751cd343aeecde63dd7e9a1f05105f32a
unknown-peter <unknownpeter@yandex.com>
2026-07-31 00:46:45 +0800

    hooks

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,13 @@
+import re
+import sys
+
+MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
+
+module_name = "{{ cookiecutter.package_name}}"
+
+if not re.match(MODULE_REGEX, module_name):
+    print(
+        f"ERROR: The package name ({module_name}) is not a valid python module name. "
+    )
+
+    sys.exit(1)
